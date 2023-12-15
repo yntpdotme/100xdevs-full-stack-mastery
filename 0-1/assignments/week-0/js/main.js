e031523e58531d9085c6dca9a83101fd68c80f9b
Akash Kadlag <akashkadlag14@gmail.com>
2023-12-15 18:53:43 +0530

    fix(week-0): address minor HTML & JS adjustments in Zerodha landing page

--- 0-1/assignments/week-0/js/main.js
+++ 0-1/assignments/week-0/js/main.js
@@ -1,13 +1,11 @@
 document.addEventListener("click", (e) => {
 	const isDropdownButton = e.target.matches("[data-dropdown-button]");
-	console.log(isDropdownButton);
 	if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return;
 
 	let currentDropdown;
 	if (isDropdownButton) {
 		console.log("Hare Krishna");
 		currentDropdown = document.querySelector(".dropdown-menu");
-		console.log(currentDropdown.classList);
 		currentDropdown.classList.toggle("active");
 	}
 
