b4481c78f22af82023f677114903b3f8b7d22d25
Akash Kadlag <akashkadlag14@gmail.com>
2023-12-15 22:43:55 +0530

    feat(week-1): implement solution for palindrome.js

--- 0-1/assignments/week-1/medium/palindrome.js
+++ 0-1/assignments/week-1/medium/palindrome.js
@@ -11,8 +11,33 @@
 */
 
 function isPalindrome(inputString) {
-	
+	// Check if the inputString is empty
+	if (inputString.trim() === "") {
+		return true;
+	}
+
+	// Clean the input string by converting to lowercase and removing non-alphanumeric characters
+	let cleanedString = inputString.toLowerCase().replace(/[\W_]/g, "");
+
+	// Check if the cleaned string is a palindrome using a loop
+	for (let i = 0; i < Math.floor(cleanedString.length / 2); i++) {
+		if (cleanedString[i] !== cleanedString[cleanedString.length - 1 - i]) {
+			return false;
+		}
+	}
+
+	return true;
+
+	/* 
+    Way-2:
+    return cleanedString === [...cleanedString].reverse().join(""); 
+      -- This method creates an array from the cleaned string, reverses it, and joins it back into a string. Then, it compares it with the original cleaned string.
+  */
 }
 
+// Example usage
+console.log(isPalindrome("Able, was I ere I saw Elba!")); // true
+console.log(isPalindrome("Madam, I'm Adam")); // true
+console.log(isPalindrome("Hello, world!")); // false
 
 module.exports = isPalindrome;
