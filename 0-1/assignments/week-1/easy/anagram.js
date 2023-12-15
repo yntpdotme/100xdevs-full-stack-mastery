1e665cf28e7902a1489baaa7828511a88547f09e
Akash Kadlag <akashkadlag14@gmail.com>
2023-12-15 22:19:26 +0530

    feat(week-1): add javascript fundamentals assignments and tests

---
+++ 0-1/assignments/week-1/easy/anagram.js
@@ -0,0 +1,22 @@
+/*
+  Question:
+  Write a function isAnagram that takes two parameters and returns true/false if they are anagrams or not. An anagram is a word, phrase, or name formed by rearranging the letters of another.
+
+  Description:
+  An anagram is a word, phrase, or name formed by rearranging the letters of another, such as "spar" and "rasp".
+  
+  Function: isAnagram
+  Description: Checks if two strings are anagrams.
+
+  @param {string} str1 - The first string.
+  @param {string} str2 - The second string.
+  
+  @returns {boolean} True if the strings are anagrams, false otherwise.
+*/
+
+function isAnagram(string1, string2) {
+	
+}
+
+
+module.exports = isAnagram;
---
+++ 0-1/assignments/week-1/medium/count-vowels.js
@@ -0,0 +1,18 @@
+/*
+  Question:
+	Implement a function countVowels that takes a string as an argument and returns the number of vowels in the string. Consider both uppercase and lowercase vowels ('a', 'e', 'i', 'o', 'u').
+
+	Description:
+	Function: countVowels
+  Description: Counts the number of vowels in a string, considering both uppercase and lowercase vowels.
+
+	@param {inputString} str - The string to be analyzed.
+	@returns {number} The number of vowels in the string.
+*/
+
+function countVowels(inputString) {
+	
+}
+
+
+module.exports = countVowels;
