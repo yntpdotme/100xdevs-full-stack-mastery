4776f783c61b96ad55bd289b945dc13d6c233ef5
Akash Kadlag <akashkadlag14@gmail.com>
2023-12-15 22:42:48 +0530

    feat(week-1): implement solution for find-largest-number.js

--- 0-1/assignments/week-1/easy/find-largest-number.js
+++ 0-1/assignments/week-1/easy/find-largest-number.js
@@ -11,8 +11,29 @@
 */
 
 function findLargestElement(array) {
-	
+	// Check if the array is empty, return undefined
+	if (array.length === 0) {
+		return undefined;
+	}
+
+	// Way-1: Sort the array ascendingly and get the first element
+	let sortedLargest = array.sort((a, b) => b - a)[0];
+
+	// Way-2: Use reduce to find the largest element
+	let reducedLargest = array.reduce((largest, current) => {
+		return largest < current ? current : largest;
+	}, array[0]);
+
+	// Way-3: Use Math.max with spread operator
+	let maxLargest = Math.max(...array);
+
+	// For this example, we will use Math.max() for its conciseness.
+	return maxLargest;
 }
 
+// Example usage
+console.log(findLargestElement([3, 7, 2, 9, 1])); // Output: 9
+console.log(findLargestElement([])); // Output: undefined
 
+// Export the function for reuse in other modules
 module.exports = findLargestElement;
