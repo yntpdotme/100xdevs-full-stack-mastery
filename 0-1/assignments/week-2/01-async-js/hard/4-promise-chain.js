5e9a46a0d05df10f41c99b08e57ac89007b5c96e
Akash Kadlag <akashkadlag14@gmail.com>
2023-12-23 18:00:22 +0530

    feat(week-2): implement sequential promise execution time measurement

--- 0-1/assignments/week-2/01-async-js/hard/4-promise-chain.js
+++ 0-1/assignments/week-2/01-async-js/hard/4-promise-chain.js
@@ -9,20 +9,48 @@
 */
 
 
+// Function that returns a promise resolving after t seconds
 const wait1 = (t) => {
-	
+	return new Promise((resolve) => {
+		setTimeout(() => resolve(t), t * 1000);
+	});
 };
 
+// Function that returns a promise resolving after t seconds
 const wait2 = (t) => {
-	
+	return new Promise((resolve) => {
+		setTimeout(() => resolve(t), t * 1000);
+	});
 };
 
+// Function that returns a promise resolving after t seconds
 const wait3 = (t) => {
-	
+	return new Promise((resolve) => {
+		setTimeout(() => resolve(t), t * 1000);
+	});
 };
 
 const calculateTime = (t1, t2, t3) => {
-	
+	// Record the start time using Date.now()
+	const start = Date.now();
+
+	// Sequentially chain promises using .then()
+	return wait1(t1)
+		.then(() => wait2(t2))
+		.then(() => wait3(t3))
+		.then(() => Date.now() - start); // Calculate the total time taken.
 }
 
+// Example usage
+calculateTime(1, 2, 3)
+	.then((totalTime) => {
+		// Log or use the total time as needed
+		console.log(`Total time taken: ${totalTime} milliseconds`);
+	})
+	// Handle any potential errors in the catch block
+	.catch((error) => {
+		console.error(`An error occurred: ${error}`);
+	});
+
+// Export the function for external use
 module.exports = calculateTime;
