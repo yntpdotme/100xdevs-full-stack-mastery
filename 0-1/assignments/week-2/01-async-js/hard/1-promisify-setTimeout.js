35eb140d7ebb2312a558d700d12b837091ba43e4
Akash Kadlag <akashkadlag14@gmail.com>
2023-12-23 17:56:58 +0530

    feat(week-2):  implement solution for promisify-setTimeout.js

--- 0-1/assignments/week-2/01-async-js/hard/1-promisify-setTimeout.js
+++ 0-1/assignments/week-2/01-async-js/hard/1-promisify-setTimeout.js
@@ -3,7 +3,24 @@
 */
 
 const wait = (n) => {
-	
+	// Create a new Promise that resolves after n seconds using setTimeout
+	return new Promise((resolve) => {
+		// Set a timeout to resolve the promise after n seconds
+		setTimeout(() => resolve(), n * 1000);
+	});
 }
 
+// Example usage
+let n = 2;
+wait(n)
+	.then(() => {
+		// Log a message when the promise is resolved
+		console.log(`Promise resolved after ${n} seconds`);
+	})
+	.catch((error) => {
+		// Log an error message if the promise is rejected
+		console.error(`Promise rejected after ${n} seconds`);
+	});
+
+// Export the wait function for external use
 module.exports = wait;
