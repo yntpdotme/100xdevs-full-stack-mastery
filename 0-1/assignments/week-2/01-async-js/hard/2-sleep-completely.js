4220dc489569fac4aa523c41da067d5676caf1bf
Akash Kadlag <akashkadlag14@gmail.com>
2023-12-23 17:58:17 +0530

    feat(week-2): implement function for busy waiting with specified milliseconds

--- 0-1/assignments/week-2/01-async-js/hard/2-sleep-completely.js
+++ 0-1/assignments/week-2/01-async-js/hard/2-sleep-completely.js
@@ -5,7 +5,23 @@
 */
 
 const sleep = (milliseconds) => {
-	
+	// Create a new Promise that resolves after the specified milliseconds using setTimeout
+	return new Promise((resolve) => {
+		setTimeout(() => resolve(), milliseconds);
+	});
 }
 
+// Example usage
+let n = 3000;
+sleep(n)
+	.then(() => {
+		// Log a message when the promise is resolved
+		console.log(`Function sleeps for ${n} seconds`);
+	})
+	.catch((error) => {
+		// Log an error message if the promise is rejected
+		console.error(`An error occurred during sleep`);
+	});
+
+// Export the sleep function for external use
 module.exports = sleep;
