306bfc0816bef2518881ad16a8c7f02aac6f3749
Akash Kadlag <akashkadlag14@gmail.com>
2023-12-23 17:26:14 +0530

    feat(week-2): implement solution for 1-counter.js

--- 0-1/assignments/week-2/01-async-js/easy/1-counter.js
+++ 0-1/assignments/week-2/01-async-js/easy/1-counter.js
@@ -2,3 +2,17 @@
   JavaScript Interval Counter:
   Create a JavaScript program that functions as a counter. The counter should increment at intervals of 1 second, continuously increasing as time progresses.
 */
+
+const startCounter = () => {
+	let count = 0;
+
+	// Set interval for recurring increments
+	const counterInterval = setInterval(() => process.stdout.write(`\r${count++}`), 1000);
+
+	// Optional: Add cleanup logic (clearInterval) if needed
+	// const durationInSeconds = 10;
+	// setTimeout(() => clearInterval(counterInterval), durationInSeconds * 1000);
+};
+
+// Start the counter
+startCounter();
