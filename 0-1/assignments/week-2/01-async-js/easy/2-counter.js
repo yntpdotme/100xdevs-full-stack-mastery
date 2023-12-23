4819d7beaf0093c0e9576e5a247115cab47344dd
Akash Kadlag <akashkadlag14@gmail.com>
2023-12-23 17:26:26 +0530

    feat(week-2): implement solution for 2-counter.js

--- 0-1/assignments/week-2/01-async-js/easy/2-counter.js
+++ 0-1/assignments/week-2/01-async-js/easy/2-counter.js
@@ -2,3 +2,16 @@
   Counter without setInterval
   Create a JavaScript program to implement a counter that increments at intervals of 1 second. The challenge is to achieve this without using the common `setInterval` function.
 */
+
+let count = 0;
+
+// Function to update the counter and display it
+const updateCounter = () => {
+	process.stdout.write(`\r${++count}`); // Increment and display the counter
+
+	// Schedule the next update using setTimeout
+	setTimeout(updateCounter, 1000);
+};
+
+// Start the counter using setTimeout
+updateCounter();
