006f230cce0df5c1e70830e6fda3cd0af3b0d2b0
Akash Kadlag <akashkadlag14@gmail.com>
2023-12-31 14:59:12 +0530

    feat(week-3): add authentication and mongodb assignments with tests

---
+++ 0-1/assignments/week-3/01-middlewares/1-request-count.js
@@ -0,0 +1,28 @@
+const request = require('supertest');
+const assert = require('assert');
+const express = require('express');
+
+const app = express();
+let requestCount = 0;
+
+/* 
+  Request Counter:
+  You have been provided with an Express server containing several endpoints.
+  Your task is to implement a global middleware using `app.use` that maintains
+  a count of the total number of requests made to the server. 
+  The count should be stored in the global variable `requestCount`.
+*/
+
+app.get('/user', function(req, res) {
+  res.status(200).json({ name: 'john' });
+});
+
+app.post('/user', function(req, res) {
+  res.status(200).json({ msg: 'created dummy user' });
+});
+
+app.get('/requestCount', function(req, res) {
+  res.status(200).json({ requestCount });
+});
+
+module.exports = app;
