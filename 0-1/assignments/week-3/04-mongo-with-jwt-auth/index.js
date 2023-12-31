0c6141cb63bb492a6b07ca21ead5559b85dad11d
Akash Kadlag <akashkadlag14@gmail.com>
2023-12-31 16:14:05 +0530

    feat(week-3): implement JWT authentication to course selling website

--- 0-1/assignments/week-3/04-mongo-with-jwt-auth/index.js
+++ 0-1/assignments/week-3/04-mongo-with-jwt-auth/index.js
@@ -1,15 +1,16 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const adminRouter = require("./routes/admin")
+require("express-async-errors");
+const express = require("express");
+const adminRouter = require("./routes/admin");
 const userRouter = require("./routes/user");
+const app = express();
 
 // Middleware for parsing request bodies
-app.use(bodyParser.json());
-app.use("/admin", adminRouter)
-app.use("/user", userRouter)
+app.use(express.json());
+app.use("/admin", adminRouter);
+app.use("/user", userRouter);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+	console.log(`Server is running on port ${PORT}`);
 });
+
--- 0-1/assignments/week-3/04-mongo-with-jwt-auth/middleware/admin.js
+++ 0-1/assignments/week-3/04-mongo-with-jwt-auth/middleware/admin.js
@@ -1,7 +1,19 @@
+const jwt = require("jsonwebtoken");
+const secret = require("../config/default.json");
+
 // Middleware for handling auth
-function adminMiddleware(req, res, next) {
-    // Implement admin auth logic
-    // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
-}
+const adminMiddleware = async (req, res, next) => {
+	const token = req.headers.authorization;
+	if (!token) return res.status(401).send("Access Denied. No token provided.");
+
+	try {
+		const jwtToken = token.split(" ")[1];
+		const decoded = jwt.verify(jwtToken, secret.jwtSecret);
+		req.admin = decoded;
+		next();
+	} catch (error) {
+		res.status(400).send("Invalid Token");
+	}
+};
 
 module.exports = adminMiddleware;
