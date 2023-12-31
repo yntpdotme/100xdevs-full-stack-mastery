9050889edeed50d6e83b0e93daa623fd57139748
Akash Kadlag <akashkadlag14@gmail.com>
2023-12-31 15:06:58 +0530

    feat(week-3): implement course selling website routes and functionality

--- 0-1/assignments/week-3/03-mongo/middleware/user.js
+++ 0-1/assignments/week-3/03-mongo/middleware/user.js
@@ -1,6 +1,14 @@
-function userMiddleware(req, res, next) {
-    // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
-}
+const { User } = require("../db");
+
+const userMiddleware = async (req, res, next) => {
+	const user = await User.findOne({
+		username: req.headers.username,
+		password: req.headers.password,
+	});
+
+	if (!user) return res.status(400).send(`Invalid Email or Password.`);
+
+	next();
+};
 
 module.exports = userMiddleware;
---
+++ 0-1/assignments/week-3/03-mongo/middleware/validation.js
@@ -0,0 +1,24 @@
+const zod = require("zod");
+
+const validate = (user) => {
+	const schema = zod.object({
+		username: zod.string().min(2),
+		password: zod.string().min(5),
+  });
+  
+  return schema.safeParse(user);
+};
+
+const validateCourse = (course) => {
+	const schema = zod.object({
+		title: zod.string().min(2),
+		description: zod.string().min(5),
+		image: zod.string().trim().url(),
+		price: zod.number().nonnegative(),
+	});
+  
+  return schema.safeParse(course);
+};
+
+module.exports.validate = validate;
+module.exports.validateCourse = validateCourse;
