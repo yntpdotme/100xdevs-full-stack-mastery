c8f09db26c5a7bdf864855d24825717458e13b72
Akash Kadlag <akashkadlag14@gmail.com>
2024-02-23 11:39:59 +0530

    feat(week-7): implement profile component

--- 0-1/assignments/week-7/src/components/Profile/Profile.jsx
+++ 0-1/assignments/week-7/src/components/Profile/Profile.jsx
@@ -1,9 +1,48 @@
+import "./Profile.css";
 
-
-const Profile = () => {
+const Profile = ({
+	avatar,
+	cover,
+	name,
+	age,
+	city,
+	followers,
+	likes,
+	posts,
+}) => {
 	return (
-		<div>Profile</div>
-	)
-}
+		<div className="profile">
+			<div className="profile__image">
+				<img src={cover} alt="cover pic" className="profile__cover-image" />
+				<img src={avatar} alt="profile pic" className="profile__avatar-image" />
+			</div>
 
-export default Profile
+			<div className="profile__details">
+				<p className="profile__name">
+					{name}
+					<span className="profile__age">{age}</span>
+				</p>
+				<p className="profile__city">{city}</p>
+				<div className="profile__stats">
+					<p>
+						<span className="profile__stats-data">{followers}</span>
+						<br />
+						Followers
+					</p>
+					<p>
+						<span className="profile__stats-data">{likes}</span>
+						<br />
+						Likes
+					</p>
+					<p>
+						<span className="profile__stats-data">{posts}</span>
+						<br />
+						Posts
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Profile;
