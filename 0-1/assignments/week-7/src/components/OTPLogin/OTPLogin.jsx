d2cae0bfc81a4c1bfadad744969e1ccfd4112a8b
Akash Kadlag <akashkadlag14@gmail.com>
2024-02-24 18:45:21 +0530

    feat(week-7): implement otp login component

--- 0-1/assignments/week-7/src/components/OTPLogin/OTPLogin.jsx
+++ 0-1/assignments/week-7/src/components/OTPLogin/OTPLogin.jsx
@@ -1,9 +1,141 @@
-
+import React, { useEffect, useRef, useState } from "react";
+import "./OTPLogin.css";
 
 const OTPLogin = () => {
-	return (
-		<div>OTPLogin</div>
-	)
-}
+	const [phoneNumber, setPhoneNumber] = useState("");
+	const [showOtpInput, setShowOtpInput] = useState(false);
+	const [error, setError] = useState("");
+	const inputRef = useRef(null);
 
-export default OTPLogin
+	const handlePhoneSubmit = (e) => {
+		e.preventDefault();
+		setError("");
+
+		// Phone Validations
+		const regex = /[^0-9]/g;
+		if (phoneNumber.length < 10 || regex.test(phoneNumber)) {
+			setError("Invalid Phone Number");
+			return;
+		}
+
+		// BackEnd API Call
+
+		// Show OTP Field
+		setShowOtpInput(true);
+	};
+
+	const handlePhoneNumberChange = (e) => {
+		setPhoneNumber(e.target.value);
+	};
+
+	const handleKeyDown = (e) => {
+		if (e.key === "Enter") handlePhoneSubmit(e);
+	};
+
+	const onOtpSubmit = (otp) => {
+		console.log("Login Successful", otp);
+	};
+
+	useEffect(() => {
+		if (inputRef.current) inputRef.current.focus();
+	}, []);
+
+	return (
+		<div className="OTPLogin">
+			{!showOtpInput ? (
+				<form onSubmit={handlePhoneSubmit}>
+					<div className="OTPLogin__input">
+						<input
+							type="number"
+							value={phoneNumber}
+							onChange={handlePhoneNumberChange}
+							onKeyDown={handleKeyDown}
+							ref={inputRef}
+							placeholder="Enter Phone Number"
+							className="spin-button-none"
+						/>
+						<button type="submit">Get OTP</button>
+					</div>
+					{error && <div className="otp-login__error">{error}</div>}
+				</form>
+			) : (
+				<div>
+					<p>Enter OTP sent to *******{phoneNumber.slice(-3)}</p>
+					<OtpInput onOtpSubmit={onOtpSubmit} />
+				</div>
+			)}
+		</div>
+	);
+};
+
+const OtpInput = ({ length = 4, onOtpSubmit }) => {
+	const [otp, setOtp] = useState(new Array(length).fill(""));
+	const inputRefs = useRef([]);
+
+	useEffect(() => {
+		if (inputRefs.current[0]) {
+			inputRefs.current[0].focus();
+		}
+	}, []);
+
+	const handleOnChange = (e, index) => {
+		const newOtp = [...otp];
+		newOtp[index] = e.target.value.slice(-1);
+
+		setOtp(newOtp);
+
+		// Submit trigger
+		const combinedOtp = newOtp.join("");
+		if (combinedOtp.length === length) onOtpSubmit(combinedOtp);
+
+		// Move to the next input if the current field is filled
+		if (e.target.value && index < length - 1 && inputRefs.current[index + 1]) {
+			let nextIndex = index + 1;
+			while (nextIndex < length && newOtp[nextIndex]) {
+				nextIndex++;
+			}
+
+			if (nextIndex < length && inputRefs.current[nextIndex]) {
+				inputRefs.current[nextIndex].focus();
+			}
+		}
+	};
+
+	const handleKeyDown = (e, index) => {
+		if (e.key === "Backspace" && !otp[index] && index > 0) {
+			// Move focus to the previous input field on backspace
+			e.preventDefault();
+			inputRefs.current[index - 1].focus();
+		}
+	};
+
+	const handleOnClick = (index) => {
+		// Select the text inside the input
+		inputRefs.current[index].select();
+
+		if (index > 0 && !otp[index - 1]) {
+			inputRefs.current[otp.indexOf("")].focus();
+		}
+	};
+
+	return (
+		<div className="OTP__input">
+			{otp.map((value, index) => (
+				<React.Fragment key={index}>
+					<input
+						type="number"
+						value={value}
+						ref={(input) => (inputRefs.current[index] = input)}
+						onChange={(e) => handleOnChange(e, index)}
+						onClick={() => handleOnClick(index)}
+						onKeyDown={(e) => handleKeyDown(e, index)}
+						className="otpInput spin-button-none"
+					/>
+					{index === length - 1 ? null : <span className="otp-input__dash" />}
+				</React.Fragment>
+			))}
+		</div>
+	);
+};
+
+export default OTPLogin;
---
+++ 0-1/assignments/week-7/src/components/OTPLogin/README.md
@@ -0,0 +1,25 @@
+## My Implementation
+
+<h3 align="center">
+
+[🗂️ Source Code](./OTPLogin.jsx)
+
+<h3>
+
+<p align="center">
+ <img width = "600px" alt="Jio Network blocking the view? Network switch reveals the magic!" src="../../assets/final-output/otp-login-component.jpg">
+ <br>
+ <img width = "600px" alt="Jio Network blocking the view? Network switch reveals the magic!" src="../../assets/final-output/otp-login-output.jpg">
+</p>
+
+<br>
+
+## Workflow
+
+- `main.jsx` component renders `App.jsx` component
+
+- `App.jsx` handles routing with React Router.
+
+- `App.jsx` component renders `HomePage.jsx` component which displays buttons linking to various components. Clicking a button navigates to the respective component page.
+
+- `OTPLoginPage.jsx` renders `OTPLogin.jsx` component. It also has Back button navigates back to `HomePage.jsx`.
