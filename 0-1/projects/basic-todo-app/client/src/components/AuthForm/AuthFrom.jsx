48998d77a65ed9f68231448c2cc331488955d0df
Akash Kadlag <akashkadlag14@gmail.com>
2024-03-07 00:14:09 +0530

    chore(todo-app): enhance AuthForm component with react-hook-form and zod validations

--- 0-1/projects/basic-todo-app/client/src/components/AuthForm/AuthFrom.jsx
+++ 0-1/projects/basic-todo-app/client/src/components/AuthForm/AuthFrom.jsx
@@ -1,52 +1,89 @@
-import { useState } from 'react';
-import './AuthForm.css'
+import {useForm} from 'react-hook-form';
+import {zodResolver} from '@hookform/resolvers/zod';
+import './AuthForm.css';
 
-const AuthForm = ({onSubmit, buttonText, isSignUp}) => {
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
+import z from 'zod';
+
+const signUpSchema = z
+  .object({
+    name: z.string({required_error: 'Name is required'}).min(3).max(255),
+    email: z.string({required_error: 'Email is required'}).email(),
+    password: z
+      .string({required_error: 'Password is required'})
+      .min(
+        6,
+        'Password must have at least 6 characters, one uppercase letter, one lowercase letter, one digit, and one special character.'
+      )
+      .refine(password => {
+        const passwordRegex =
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/;
+        return passwordRegex.test(password);
+      }, 'Password must have at least 6 characters, one uppercase letter, one lowercase letter, one digit, and one special character.'),
+
+    confirmPassword: z.string({
+      required_error: 'Confirm Password is required',
+    }),
+  })
+  .refine(data => data.password === data.confirmPassword, {
+    message: 'Passwords do not match',
+    path: ['confirmPassword'],
   });
 
-  const handleChange = e => {
-    setFormData({...formData, [e.target.name]: e.target.value});
-  };
+const signInSchema = z.object({
+  email: z
+    .string({required_error: 'Email is required'})
+    .email('Please enter a valid email address'),
+  password: z
+    .string({required_error: 'Password is required'})
+    .min(1, 'Password is required'),
+});
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    onSubmit(formData);
+const AuthForm = ({onSubmit, buttonText, isSignUp}) => {
+  const {
+    register,
+    handleSubmit,
+    formState: {errors, isValid},
+  } = useForm({
+    resolver: zodResolver(isSignUp ? signUpSchema : signInSchema),
+    mode: 'onChange',
+  });
+
+  const onSubmitHandler = data => {
+    onSubmit(data);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmitHandler)}>
+      {isSignUp && (
+        <>
+          <input {...register('name')} type="text" placeholder="Name" />
+          {errors.name?.message && (
+            <p className="error">{errors.name.message}</p>
+          )}
+        </>
+      )}
+      <input {...register('email')} type="email" placeholder="Email" />
+      {errors.email?.message && <p className="error">{errors.email.message}</p>}
+      <input {...register('password')} type="password" placeholder="Password" />
+      {errors.password?.message && (
+        <p className="error">{errors.password.message}</p>
+      )}
       {isSignUp && (
         <>
           <input
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            placeholder="Name"
-            required
+            {...register('confirmPassword')}
+            type="password"
+            placeholder="Confirm Password"
           />
+          {errors.confirmPassword?.message && (
+            <p className="error">{errors.confirmPassword.message}</p>
+          )}
         </>
       )}
-      <input
-        type="email"
-        name="email"
-        value={formData.email}
-        onChange={handleChange}
-        placeholder="Email"
-        required
-      />
-      <input
-        type="password"
-        name="password"
-        value={formData.password}
-        onChange={handleChange}
-        placeholder="Password"
-        required
-      />
-      <button type="submit">{buttonText}</button>
+      {errors.message && <p className="error">{errors.message}</p>}
+      <button type="submit" disabled={!isValid}>
+        {buttonText}
+      </button>
     </form>
   );
 };
