cc4ddbea39e733675ba6044b54f798b4ceb0734d
Akash Kadlag <akashkadlag14@gmail.com>
2024-03-07 01:14:08 +0530

    chore(todo-app): add production middlewares for enhanced backend security

--- 0-1/projects/basic-todo-app/server/server.js
+++ 0-1/projects/basic-todo-app/server/server.js
@@ -2,9 +2,11 @@ import express from 'express';
 
 import routes from './startup/routes.js';
 import db from './startup/db.js';
+import prod from './startup/prod.js';
 
 const app = express();
 
+prod(app);
 routes(app);
 db();
 
