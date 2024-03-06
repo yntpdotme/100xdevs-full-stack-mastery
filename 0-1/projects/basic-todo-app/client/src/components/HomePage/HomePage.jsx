29816be3c2a96955ec106445b2f090053f230c74
Akash Kadlag <akashkadlag14@gmail.com>
2024-03-07 00:21:43 +0530

    chore(todo-app): enhance HomePage component

--- 0-1/projects/basic-todo-app/client/src/components/HomePage/HomePage.jsx
+++ 0-1/projects/basic-todo-app/client/src/components/HomePage/HomePage.jsx
@@ -1,15 +1,16 @@
 import {Link} from 'react-router-dom';
 import reactLogo from '../../assets/react.svg';
-import './HomePage.css'
+import './HomePage.css';
 
 const HomePage = () => {
   return (
     <div>
-      <h1 className='headline'>Todo App</h1>
       <a href="https://react.dev" target="_blank">
         <img src={reactLogo} className="logo react" alt="React logo" />
       </a>
-      <p className='tagline'>Be Kind To Your Mind</p>
+      <p className="tagline">Be kind to your mind with our</p>
+      <h1 className="headline">Todo App</h1>
+      <br />
       <Link to="/signup">
         <button>Get Started</button>
       </Link>
