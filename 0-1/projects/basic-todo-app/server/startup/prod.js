831057cf7cf3974ef9453e5bf80c41b46ce40e73
Akash Kadlag <akashkadlag14@gmail.com>
2024-03-07 01:14:08 +0530

    chore(todo-app): add production middlewares for enhanced backend security

---
+++ 0-1/projects/basic-todo-app/server/startup/prod.js
@@ -0,0 +1,27 @@
+import helmet from 'helmet';
+import compression from 'compression';
+import hpp from 'hpp';
+import rateLimit from 'express-rate-limit';
+import cors from 'cors';
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
+
+const corsOptions = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+const prod = app => {
+  app.use(cors(corsOptions));
+  app.use(helmet());
+  app.use(compression());
+  app.use(hpp());
+  app.use('/api/', limiter);
+};
+
+export default prod;
