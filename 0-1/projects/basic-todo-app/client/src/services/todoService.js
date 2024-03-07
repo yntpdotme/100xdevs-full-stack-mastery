a1ea24118313e118a4c74708208f6eb25e0e22d7
Akash Kadlag <akashkadlag14@gmail.com>
2024-03-07 16:32:41 +0530

    feat(todo-app): implement todo service

---
+++ 0-1/projects/basic-todo-app/client/src/services/todoService.js
@@ -0,0 +1,27 @@
+import {apiClientAuthenticated} from './apiClient';
+
+const todoService = {
+  getAllTodos: () => {
+    const controller = new AbortController();
+
+    const request = apiClientAuthenticated.get('/todos', {
+      signal: controller.signal,
+    });
+
+    return {request, cancel: () => controller.abort()};
+  },
+
+  deleteTodo: id => {
+    return apiClientAuthenticated.delete('/todos/' + id);
+  },
+
+  createTodo(todo) {
+    return apiClientAuthenticated.post('/todos/', todo);
+  },
+
+  updateTodo(todo) {
+    return apiClientAuthenticated.put('/todos/' + todo.id, todo);
+  },
+};
+
+export default todoService;
