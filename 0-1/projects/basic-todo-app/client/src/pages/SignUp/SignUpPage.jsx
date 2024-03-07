ad72fd2a1bdaef5871a90b5299388bc88bf673ef
Akash Kadlag <akashkadlag14@gmail.com>
2024-03-07 16:20:56 +0530

    feat(todo-app): integrate signup functionality with backend API

--- 0-1/projects/basic-todo-app/client/src/pages/SignUp/SignUpPage.jsx
+++ 0-1/projects/basic-todo-app/client/src/pages/SignUp/SignUpPage.jsx
@@ -1,24 +1,24 @@
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
+
 import {AuthForm} from '../../components';
-import api from '../../services/api';
+import signupService from '../../services/signupService';
+
 import './SignUpPage.css';
 
 const SignUpPage = () => {
+  const navigate = useNavigate();
+
   const handleSignUp = async formData => {
-    try {
-      const response = await api.post('/signup', formData);
-      console.log('User registered successfully:', response.data);
-      // Redirect to login or perform any other action after signup
-    } catch (error) {
-      console.error('Error signing up:', error.response.data);
-      // Handle signup error
-    }
+    const response = await signupService.signUp(formData);
+    console.log('User registered successfully:', response);
+
+    navigate('/todos');
   };
 
   return (
     <div>
       <h1>Create Account</h1>
-      <AuthForm onSubmit={handleSignUp} buttonText="Create account" isSignUp />
+      <AuthForm onSubmit={handleSignUp} buttonText="Create my account" isSignUp />
       <p className="footer">
         Have an account? <Link to="/signin">Sign In</Link>
       </p>
