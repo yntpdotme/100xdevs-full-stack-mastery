8c32f592fa7dd698b265cc7db4c58dbd7e8bd62b
Akash Kadlag <akashkadlag14@gmail.com>
2024-03-09 00:54:28 +0530

    fix(todo-app): resolve authorization token sending issues

--- 0-1/projects/basic-todo-app/client/src/services/apiClient.js
+++ 0-1/projects/basic-todo-app/client/src/services/apiClient.js
@@ -3,7 +3,6 @@ import axios, {CanceledError} from 'axios';
 import {getAccessToken} from './tokenStroage';
 
 const baseURL = import.meta.env.VITE_API_BASE_URL;
-const token = getAccessToken();
 
 export default axios.create({
   baseURL,
@@ -12,7 +11,16 @@ export default axios.create({
 export const apiClientAuthenticated = axios.create({
   baseURL,
   withCredentials: true,
-  headers: {Authorization: `Bearer ${token}`},
 });
+
+apiClientAuthenticated.interceptors.request.use(
+  function (config) {
+    config.headers = {Authorization: `Bearer ${getAccessToken()}`};
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
 
 export {CanceledError};
