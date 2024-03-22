a4b6f07f7db8614d90839f7be2cb3cfc12fb5997
Akash Kadlag <akashkadlag14@gmail.com>
2024-03-22 14:18:33 +0530

    feat(wallet-app): implement user registration with JWT

---
+++ 0-1/projects/basic-wallet-app/server/validators/user.validators.js
@@ -0,0 +1,47 @@
+import z from 'zod';
+
+const userRegisterValidator = user => {
+  const schema = z
+    .object({
+      name: z
+        .string({
+          required_error: 'Name is required',
+          invalid_type_error: 'Name must be a string',
+        })
+        .trim()
+        .min(3, {message: 'Must be 3 or more characters long'})
+        .max(255, {message: 'Must be 255 or fewer characters long'}),
+
+      email: z
+        .string({
+          required_error: 'Email is required',
+        })
+        .email('Please enter a valid email address'),
+
+      password: z
+        .string({required_error: 'Password is required'})
+        .min(
+          6,
+          'Password must have at least 6 characters, one uppercase letter, one lowercase letter, one digit, and one special character.'
+        )
+        .refine(password => {
+          // At least one uppercase letter, one lowercase letter, one digit, and one special character
+          const passwordRegex =
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/;
+          return passwordRegex.test(password);
+        }, 'Password must have at least 6 characters, one uppercase letter, one lowercase letter, one digit, and one special character.'),
+
+      passwordConfirmation: z.string({
+        required_error: 'Password Confirmation is required',
+      }),
+    })
+    .strict()
+    .refine(data => data.password === data.passwordConfirmation, {
+      message: 'Passwords do not match',
+      path: ['passwordConfirmation'],
+    });
+
+  return schema.safeParse(user);
+};
+
+export { userRegisterValidator as ValidateRegister };
