4c4385ff8f5cf7c622ff7d6080f81cec96dc994a
Akash Kadlag <akashkadlag14@gmail.com>
2024-03-22 22:30:02 +0530

    feat(wallet-app): implement Read and Update operations for users api endpoint

--- 0-1/projects/basic-wallet-app/server/validators/user.validators.js
+++ 0-1/projects/basic-wallet-app/server/validators/user.validators.js
@@ -31,14 +31,14 @@ const userRegisterValidator = user => {
           return passwordRegex.test(password);
         }, 'Password must have at least 6 characters, one uppercase letter, one lowercase letter, one digit, and one special character.'),
 
-      passwordConfirmation: z.string({
+      confirmPassword: z.string({
         required_error: 'Password Confirmation is required',
       }),
     })
     .strict()
-    .refine(data => data.password === data.passwordConfirmation, {
+    .refine(data => data.password === data.confirmPassword, {
       message: 'Passwords do not match',
-      path: ['passwordConfirmation'],
+      path: ['confirmPassword'],
     });
 
   return schema.safeParse(user);
@@ -59,7 +59,110 @@ const userLoginValidator = user => {
   return schema.safeParse(user);
 };
 
+const userUpdateValidator = user => {
+  const nameSchema = z
+    .object({
+      name: z
+        .string({
+          required_error: 'Name is required',
+          invalid_type_error: 'Name must be a string',
+        })
+        .trim()
+        .min(3, {message: 'Must be 3 or more characters long'})
+        .max(255, {message: 'Must be 255 or fewer characters long'}),
+    })
+    .strict();
+
+  const passwordSchema = z
+    .object({
+      oldPassword: z
+        .string({required_error: 'Old Password is required'})
+        .min(1, 'Old Password is required'),
+
+      newPassword: z
+        .string({required_error: 'New Password is required'})
+        .min(
+          6,
+          'Password must have at least 6 characters, one uppercase letter, one lowercase letter, one digit, and one special character.'
+        )
+        .refine(password => {
+          // At least one uppercase letter, one lowercase letter, one digit, and one special character
+          const passwordRegex =
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/;
+          return passwordRegex.test(password);
+        }, 'Password must have at least 6 characters, one uppercase letter, one lowercase letter, one digit, and one special character.'),
+
+      confirmNewPassword: z.string({
+        required_error: 'Please Confirm New Password',
+      }),
+    })
+    .strict()
+    .refine(
+      data =>
+        data.newPassword && data.confirmNewPassword
+          ? data.newPassword === data.confirmNewPassword
+          : true,
+      {
+        message: 'Passwords do not match',
+        path: ['confirmNewPassword'],
+      }
+    );
+
+  const schema = z
+    .object({
+      name: z
+        .string({
+          required_error: 'Name is required',
+          invalid_type_error: 'Name must be a string',
+        })
+        .trim()
+        .min(3, {message: 'Must be 3 or more characters long'})
+        .max(255, {message: 'Must be 255 or fewer characters long'}),
+
+      oldpassword: z
+        .string({required_error: 'Old Password is required'})
+        .min(1, 'Old Password is required'),
+
+      newPassword: z
+        .string({required_error: 'New Password is required'})
+        .min(
+          6,
+          'Password must have at least 6 characters, one uppercase letter, one lowercase letter, one digit, and one special character.'
+        )
+        .refine(password => {
+          // At least one uppercase letter, one lowercase letter, one digit, and one special character
+          const passwordRegex =
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/;
+          return passwordRegex.test(password);
+        }, 'Password must have at least 6 characters, one uppercase letter, one lowercase letter, one digit, and one special character.'),
+
+      confirmNewPassword: z.string({
+        required_error: 'Password Confirmation is required',
+      }),
+    })
+    .strict()
+    .refine(
+      data =>
+        data.newPassword && data.confirmNewPassword
+          ? data.newPassword === data.confirmNewPassword
+          : true,
+      {
+        message: 'Passwords do not match',
+        path: ['confirmNewPassword'],
+      }
+    );
+
+  if (!user.name) {
+    return passwordSchema.safeParse(user);
+  } else if (!user.oldpassword) {
+    return nameSchema.safeParse(user);
+  }
+
+  return schema.safeParse(user);
+};
+
 export {
   userRegisterValidator as ValidateRegister,
   userLoginValidator as ValidateLogin,
+  userUpdateValidator as ValidateUpdate,
 };
