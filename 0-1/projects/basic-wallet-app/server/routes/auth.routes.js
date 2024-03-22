21080232dbfb91e0dac9e127bb250c6af4ce6dd7
Akash Kadlag <akashkadlag14@gmail.com>
2024-03-22 15:26:25 +0530

    feat(wallet-app): implement user login with JWT

--- 0-1/projects/basic-wallet-app/server/routes/auth.routes.js
+++ 0-1/projects/basic-wallet-app/server/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 
-import {registerUser} from '../controllers/user.controllers.js';
+import {registerUser, loginUser} from '../controllers/user.controllers.js';
 
 const router = express.Router();
 
 router.route('/signup').post(registerUser);
+router.route('/signin').post(loginUser);
 
 export default router;
