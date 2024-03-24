d9ac567d54568695811f417787f70e275f267751
Akash Kadlag <akashkadlag14@gmail.com>
2024-03-24 17:33:17 +0530

    chore(wallet-app): add production middlewares for enhanced backend security

---
+++ 0-1/projects/basic-wallet-app/server/startup/production.middlewares.js
@@ -0,0 +1,43 @@
+import cors from 'cors';
+import requestIp from 'request-ip';
+import helmet from 'helmet';
+import compression from 'compression';
+import hpp from 'hpp';
+import rateLimit from 'express-rate-limit';
+
+const corsOptions = {
+  origin:
+    process.env.CORS_ORIGIN === '*' ? '*' : process.env.CORS_ORIGIN?.split(','),
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 300, // Limit each IP to 300 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  keyGenerator: (req, res) => {
+    return req.clientIp; // IP address from requestIp.mw(), as opposed to req.ip
+  },
+  handler: (_, __, ___, options) => {
+    throw new ApiError(
+      options.statusCode || 500,
+      `There are too many requests. You are only allowed ${
+        options.max
+      } requests per ${options.windowMs / 60000} minutes`
+    );
+  },
+});
+
+const prodMiddlewares = app => {
+  app.use(cors(corsOptions));
+  app.use(helmet());
+  app.use(compression());
+  app.use(hpp());
+  app.use(requestIp.mw());
+  app.use('/api/', limiter);
+};
+
+export default prodMiddlewares;
