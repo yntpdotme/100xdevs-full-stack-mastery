8410b5a82fab2c45e7dfa32a02e80aa5fbcaff66
Akash Kadlag <akashkadlag14@gmail.com>
2024-03-29 03:29:07 +0530

    refactor(wallet-app): improve HTML semantics

--- 0-1/projects/basic-wallet-app/client/src/components/PopUp.jsx
+++ 0-1/projects/basic-wallet-app/client/src/components/PopUp.jsx
@@ -4,12 +4,12 @@ import {dashboardIcon, settingsIcon, signOutIcon} from '../assets';
 
 const PopUp = () => {
   return (
-    <div className="bg-popover min-w-[14.5rem] overflow-hidden rounded-md border border-dark-800 p-1 font-light shadow-[0_30px_100px_-5px] shadow-primary/40">
+    <div className="min-w-[14.5rem] overflow-hidden rounded-md border border-dark-800 bg-popover p-1 font-light shadow-[0_30px_100px_-5px] shadow-primary/40">
       <div
-        className="focus:text-accent-foreground relative flex cursor-default select-none items-center rounded-none p-2 text-sm outline-none transition-colors hover:bg-accent"
+        className="focus:text-accent-foreground relative flex cursor-default select-none items-center rounded-none p-3 text-sm outline-none transition-colors hover:bg-accent"
         tabIndex="-1"
       >
-        <div className="flex flex-col justify-start truncate text-left text-xs">
+        <div className="flex flex-col justify-start truncate text-left text-[15px]">
           <div className="text-gray-500">Signed in as</div>
           <div>
             <span className="block truncate">user@email.com</span>
@@ -17,10 +17,10 @@ const PopUp = () => {
         </div>
       </div>
 
-      <div role="separator" className="bg-muted -mx-1 my-1 h-px"></div>
+      <div role="separator" className="-mx-1 my-1 h-px bg-muted"></div>
 
       <Link
-        className="focus:text-accent-foreground relative flex cursor-default select-none items-center space-x-2 rounded-sm p-2 text-sm outline-none transition-colors hover:bg-accent data-[disabled]:pointer-events-none data-[disabled]:opacity-50"
+        className="focus:text-accent-foreground relative flex cursor-default select-none items-center space-x-2 rounded-sm p-3 text-[15px] outline-none transition-colors hover:bg-accent data-[disabled]:pointer-events-none data-[disabled]:opacity-50"
         tabIndex="-1"
         to="/dashboard"
       >
@@ -29,7 +29,7 @@ const PopUp = () => {
       </Link>
 
       <Link
-        className="focus:text-accent-foreground relative flex cursor-default select-none items-center space-x-2 rounded-sm p-2 text-sm outline-none transition-colors hover:bg-accent data-[disabled]:pointer-events-none data-[disabled]:opacity-50"
+        className="focus:text-accent-foreground relative flex cursor-default select-none items-center space-x-2 rounded-sm p-3 py-2.5 text-[15px] outline-none transition-colors hover:bg-accent data-[disabled]:pointer-events-none data-[disabled]:opacity-50"
         tabIndex="-1"
         to="/profile"
       >
@@ -40,11 +40,11 @@ const PopUp = () => {
       <div
         role="separator"
         aria-orientation="horizontal"
-        className="bg-muted -mx-1 my-1 hidden h-px lg:flex"
+        className="-mx-1 my-1 hidden h-px bg-muted lg:flex"
       ></div>
 
       <button
-        className="focus:text-accent-foreground relative flex w-full cursor-pointer select-none items-center rounded-sm p-2 text-sm outline-none transition-colors hover:bg-accent"
+        className="focus:text-accent-foreground relative flex w-full cursor-pointer select-none items-center rounded-sm p-3 text-[15px] outline-none transition-colors hover:bg-accent"
         tabIndex="-1"
       >
         <span className="flex items-center space-x-2">
--- 0-1/projects/basic-wallet-app/client/src/pages/SignInPage.jsx
+++ 0-1/projects/basic-wallet-app/client/src/pages/SignInPage.jsx
@@ -10,12 +10,12 @@ const SignInPage = () => {
   };
 
   return (
-    <main className="max-container flex h-screen flex-col items-center justify-center">
+    <section className="max-container flex h-screen flex-col items-center justify-center">
       <p className="mb-10 bg-gradient-to-r from-gray-600 to-white bg-clip-text font-palanquin text-2xl font-extrabold text-transparent">
         PaymentX
       </p>
 
-      <div className="flex w-full max-w-sm flex-col items-center space-y-4 rounded-xl border-transparent bg-transparent px-2 py-6 max-sm:px-1 md:w-8/12 md:border md:px-8 md:shadow-[0_0_1200px_0] md:shadow-primary/30 lg:w-7/12 lg:border-dark-800 lg:bg-background lg:px-6 xl:w-6/12 2xl:w-5/12">
+      <div className="flex w-full max-w-sm flex-col items-center space-y-4 rounded-xl border-transparent bg-transparent px-4 py-6 md:w-8/12 md:border md:px-8 md:shadow-[0_0_1200px_0] md:shadow-primary/30 lg:w-7/12 lg:border-dark-800 lg:bg-background lg:px-6 xl:w-6/12 2xl:w-5/12">
         <div>
           <h5 className="mb-2 font-palanquin text-xl font-medium">
             Sign in to your account
@@ -33,7 +33,7 @@ const SignInPage = () => {
           </p>
         </div>
       </div>
-    </main>
+    </section>
   );
 };
 
