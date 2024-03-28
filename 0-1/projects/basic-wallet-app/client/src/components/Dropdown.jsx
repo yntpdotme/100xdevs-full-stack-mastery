007ae9a702a1d16c0f0799378017ee27d1dddc25
Akash Kadlag <akashkadlag14@gmail.com>
2024-03-29 02:03:06 +0530

    feat(wallet-app): implement navbar

---
+++ 0-1/projects/basic-wallet-app/client/src/components/Dropdown.jsx
@@ -0,0 +1,38 @@
+import {signOutIcon} from '../assets';
+import {navLinks} from '../constants';
+import {NavLink} from './index';
+
+const Dropdown = () => {
+  return (
+    <div className="bg-popover absolute top-[75px] mx-3 w-[calc(100vw-2rem)] overflow-hidden rounded-md border border-dark-800 px-1 py-3 font-light shadow-[0_30px_60px_-16px] shadow-primary/40">
+      <div className="flex flex-col space-y-3 overflow-y-auto px-1 font-montserrat">
+        {navLinks.map((navLink, index) => (
+          <NavLink
+            label={navLink.label}
+            icon={navLink.icon}
+            link={navLink.link}
+            key={index}
+          />
+        ))}
+
+        <div
+          role="separator"
+          aria-orientation="horizontal"
+          className="bg-muted -mx-1 my-1 h-px"
+        ></div>
+
+        <button
+          className="focus:text-accent-foreground relative flex cursor-pointer select-none items-center rounded-sm px-4 py-2 text-sm outline-none transition-colors hover:bg-accent"
+          tabIndex="-1"
+        >
+          <span className="flex items-center space-x-3">
+            <img src={signOutIcon} className="h-6" />
+            <span>Sign out</span>
+          </span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;
---
+++ 0-1/projects/basic-wallet-app/client/src/components/NavBar.jsx
@@ -0,0 +1,27 @@
+import {Link} from 'react-router-dom';
+import {useState} from 'react';
+
+import {hamburgerMenu} from '../assets';
+import {Dropdown} from '.';
+
+const NavBar = () => {
+  const [showDropDrown, setShowDropDown] = useState(false);
+
+  return (
+    <nav className="lg:hidden">
+      <div className="flex items-start justify-between p-4 pt-6">
+        <div className="bg-gradient-to-r from-gray-600 to-white bg-clip-text font-palanquin text-2xl font-extrabold text-transparent">
+          <Link to="/">PaymentX</Link>
+        </div>
+
+        <button onClick={() => setShowDropDown(!showDropDrown)}>
+          <img src={hamburgerMenu} alt="hamburger menu" className="h-8" />
+        </button>
+      </div>
+
+      {showDropDrown && <Dropdown />}
+    </nav>
+  );
+};
+
+export default NavBar;
