30182dc21f7afbb1ec084f07d86f05b57c532c27
Akash Kadlag <akashkadlag14@gmail.com>
2024-03-28 22:46:13 +0530

    feat(wallet-app): implement SideBar component

---
+++ 0-1/projects/basic-wallet-app/client/src/constants/index.js
@@ -0,0 +1,13 @@
+import {
+  dashboardIcon,
+  profileIcon,
+  transactionsIcon,
+  transferIcon,
+} from '../assets';
+
+export const navLinks = [
+  {label: 'Dashboard', icon: dashboardIcon, link: '/dashboard'},
+  {label: 'Transfer Fund', icon: transferIcon, link: '/transfer'},
+  {label: 'User Profile', icon: profileIcon, link: '/profile'},
+  {label: 'Transaction History', icon: transactionsIcon, link: '/transactions'},
+];
