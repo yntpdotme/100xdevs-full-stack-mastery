8d2120a8270e10bef8186030167dfda64b3ce596
Akash Kadlag <akashkadlag14@gmail.com>
2024-04-18 00:47:38 +0530

    feat(wallet-app): implement fetching balance from backend API

--- 0-1/projects/basic-wallet-app/client/src/main.jsx
+++ 0-1/projects/basic-wallet-app/client/src/main.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
 import {BrowserRouter} from 'react-router-dom';
 import {RecoilRoot} from 'recoil';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
+
+import App from './App.jsx';
+import './index.css';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <RecoilRoot>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+          <ReactQueryDevtools />
+        </QueryClientProvider>
       </RecoilRoot>
     </BrowserRouter>
   </React.StrictMode>,
--- 0-1/projects/basic-wallet-app/client/src/pages/DashboardPage.jsx
+++ 0-1/projects/basic-wallet-app/client/src/pages/DashboardPage.jsx
@@ -1,7 +1,19 @@
+import {useQuery} from '@tanstack/react-query';
+
+import {WalletService} from '../api/services';
 import {GainIndicator, DepositForm} from '../components';
 
 const DashboardPage = () => {
+  const {data, isLoading, isError} = useQuery({
+    queryKey: ['balance'],
+    queryFn: async () => {
+      const response = await WalletService.getBalance();
+      return response.data.data;
+    },
+  });
+
   const handleDeposit = () => {};
+
   return (
     <section className="px-2 pt-6 lg:p-6">
       <div className="flex w-full flex-col items-center space-x-2 lg:items-start lg:space-x-0">
@@ -18,12 +30,23 @@ const DashboardPage = () => {
         <h6 className="scroll-m-20 font-montserrat text-base font-medium">
           <span className="font-normal text-gray-400">Wallet Balance</span>
         </h6>
-        <div className="flex flex-col space-y-5">
-          <div className="flex justify-between">
-            <div className="text-3xl font-bold">₹ 1300</div>
-            <GainIndicator gain={10} />
+        {isLoading ? (
+          <div>
+            <div className="inline-block size-9 animate-spin rounded-full border-[5px] border-gray-200 border-t-primary dark:border-gray-600 dark:border-t-primary"></div>
+            <span className="sr-only">Loading...</span>
           </div>
-        </div>
+        ) : isError ? (
+          <div className="mt-8 text-2xl font-normal leading-tight text-red-400">
+            Your request cannot be processed at this time.
+          </div>
+        ) : (
+          <div className="flex flex-col space-y-5">
+            <div className="flex justify-between">
+              <div className="text-3xl font-bold">₹ {data?.balance}</div>
+              <GainIndicator gain={data?.gain} />
+            </div>
+          </div>
+        )}
       </div>
 
       <div className="mt-8 flex w-full flex-col rounded-md border border-gray-100 p-5 dark:border-dark-900 xl:max-w-2xl">
