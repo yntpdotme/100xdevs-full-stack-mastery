92a5c0548958d715f07fd26d643fcf005002c1fd
Akash Kadlag <akashkadlag14@gmail.com>
2024-04-20 21:14:30 +0530

    feat(wallet-app): add pagination to efficienty retrive users

---
+++ 0-1/projects/basic-wallet-app/server/utils/pagination.js
@@ -0,0 +1,30 @@
+import {ApiError} from './ApiError.js';
+
+const paginateQuery = async (query, page = 1, limit = 10) => {
+  if (page < 1 || limit < 1) {
+    throw new ApiError(400, 'Page number and limit must be positive integers.');
+	}
+	
+  const totalCount = await query.model.countDocuments(query._conditions);
+
+  const lastPage = Math.ceil(totalCount / limit);
+
+  // Adjust page number if it exceeds total pages
+  const currentPage = Math.min(page, lastPage);
+
+  const skipValue = (currentPage - 1) * limit;
+
+  const documents = await query.skip(skipValue).limit(limit).exec();
+
+  return {
+    pagination: {
+      currentPage,
+      lastPage,
+      limit,
+      totalCount,
+    },
+    data: documents,
+  };
+};
+
+export default paginateQuery;
